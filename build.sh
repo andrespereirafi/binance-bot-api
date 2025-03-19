e9b612b4947219955b85268d2ab20617f5420894
Andrés Pereira Figueroa <andres@Andres-Air.technicolor.net>
2025-03-20 00:46:35 +0100

    Agregar script para instalar TA-Lib manualmente

--- build.sh
+++ build.sh
@@ -10,5 +10,19 @@ apt-get update && apt-get install -y \
     python3-dev \
     python3-pip
 
-# Instalar TA-Lib con pip
+# Descargar e instalar TA-Lib manualmente
+mkdir -p /opt/ta-lib
+cd /opt/ta-lib
+wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.4.0-src.tar.gz
+tar -xvzf ta-lib-0.4.0-src.tar.gz
+cd ta-lib
+./configure --prefix=/usr
+make
+make install
+
+# Volver al directorio principal
+cd /opt/render/project/src
+
+# Instalar librerías de Python (sin TA-Lib en requirements.txt)
+pip install numpy pandas ccxt
 pip install TA-Lib
